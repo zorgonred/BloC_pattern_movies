d7f2520d36d142a395b7317c702a42d757337b11
Daniel de Oliveira <danielddo2@hotmail.com>
2019-10-27 13:32:15 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+//https://medium.com/flutterpub/architecting-your-flutter-project-bd04e144a8f1
+import 'package:flutter/material.dart';
+//the equivalent of home
+import 'src/app.dart';
+
+void main(){
+  runApp(App());
+}
